890e9031573c27f8227c73b64a6f58bb5529dd78
lavr <lavrentev.srg@gmail.com>
2017-04-12 23:15:25 +0300

    init commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"github.com/docker/docker/client"
+	s "github.com/lavrs/docker-monitoring-service/stats"
+	"gopkg.in/gin-gonic/gin.v1"
+)
+
+func main() {
+	r := gin.Default()
+
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	r.GET("/", func(c *gin.Context) {
+		stats := s.Stats(context.Background(), cli, "splines")
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.JSON(200, gin.H{
+			"cpu": stats.CPUPercentage,
+			"mem": stats.MemoryPercentage,
+		})
+	})
+
+	r.Run()
+}
---
+++ stats/stats.go
@@ -0,0 +1,100 @@
+package stats
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/cli/command/formatter"
+	"github.com/docker/docker/client"
+	"strings"
+)
+
+func Stats(ctx context.Context, cli *client.Client, ID string) *formatter.ContainerStats {
+	stats, err := cli.ContainerStats(ctx, ID, true)
+	if err != nil {
+		panic(err)
+	}
+	defer stats.Body.Close()
+
+	dec := json.NewDecoder(stats.Body)
+	var statsJSON *types.StatsJSON
+	dec.Decode(&statsJSON)
+
+	return convert(statsJSON)
+}
+
+func convert(statsJSON *types.StatsJSON) *formatter.ContainerStats {
+	var containerStats formatter.ContainerStats
+
+	read, write := parseBlockIO(statsJSON.BlkioStats)
+	received, sent := parseNetwork(statsJSON.Networks)
+
+	containerStats.SetStatistics(formatter.StatsEntry{
+		Name:             statsJSON.Name,
+		ID:               statsJSON.ID,
+		MemoryPercentage: memoryToPercentage(statsJSON),
+		CPUPercentage:    parseCPU(statsJSON),
+		Memory:           float64(statsJSON.MemoryStats.Usage),
+		MemoryLimit:      float64(statsJSON.MemoryStats.Limit),
+		NetworkTx:        sent,
+		NetworkRx:        received,
+		BlockRead:        float64(read),
+		BlockWrite:       float64(write),
+		PidsCurrent:      statsJSON.PidsStats.Current,
+	})
+
+	return &containerStats
+}
+
+func memoryToPercentage(statsJSON *types.StatsJSON) float64 {
+	mem := float64(statsJSON.MemoryStats.Usage) / float64(statsJSON.MemoryStats.Limit) * 100.0
+	return mem
+}
+
+func parseNetwork(net map[string]types.NetworkStats) (float64, float64) {
+	var rx, tx float64
+
+	for _, v := range net {
+		rx += float64(v.RxBytes)
+		tx += float64(v.TxBytes)
+	}
+
+	return rx, tx
+}
+
+func parseCPU(statsJSON *types.StatsJSON) float64 {
+	var (
+		cpuPercent = 0.0
+
+		cpuDelta = float64(statsJSON.CPUStats.CPUUsage.TotalUsage) - float64(statsJSON.PreCPUStats.CPUUsage.TotalUsage)
+
+		systemDelta = float64(statsJSON.CPUStats.SystemUsage) - float64(statsJSON.PreCPUStats.SystemUsage)
+		onlineCPUs  = float64(statsJSON.CPUStats.OnlineCPUs)
+	)
+
+	if onlineCPUs == 0.0 {
+		onlineCPUs = float64(len(statsJSON.CPUStats.CPUUsage.PercpuUsage))
+	}
+	if systemDelta > 0.0 && cpuDelta > 0.0 {
+		cpuPercent = (cpuDelta / systemDelta) * onlineCPUs * 100.0
+	}
+	return cpuPercent
+}
+
+func parseBlockIO(blkioStats types.BlkioStats) (uint64, uint64) {
+	var (
+		blkRead  uint64
+		blkWrite uint64
+	)
+
+	for _, blkIO := range blkioStats.IoServiceBytesRecursive {
+		switch strings.ToLower(blkIO.Op) {
+		case "read":
+			blkRead = blkRead + blkIO.Value
+		case "write":
+			blkWrite = blkWrite + blkIO.Value
+		}
+	}
+
+	return blkRead, blkWrite
+}
